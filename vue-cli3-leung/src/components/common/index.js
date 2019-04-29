6056a0c9649ccba8a658a7a45d236b1b69796d97
boliangleung <boliangleung@163.com>
2019-04-29 18:03:29 +0800

    完成vue_init一些基础化的配置

---
+++ vue-cli3-leung/src/components/common/index.js
@@ -0,0 +1,7 @@
+/**
+ * 路由表
+ * 不喜欢把公共组件放到全局下 如果需要 可以放入main.js下引用  ** lbl
+ */
+import example from './example'
+import HelloWorld from './HelloWorld'
+export { example, HelloWorld }
---
+++ vue-cli3-leung/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div>\131</div>
+    <div>dada</div>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <hello-world></hello-world>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import { HelloWorld } from '@/components/common'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
